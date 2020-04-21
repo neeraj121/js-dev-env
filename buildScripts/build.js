c37f7209015002021cad7f320aa1438aa2143b42
neeraj-grow <neeraj@grow.ae>
2020-04-21 20:47:57 +0400

    Production webpack config, distServer, package.json

---
+++ buildScripts/build.js
@@ -0,0 +1,34 @@
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import chalk from 'chalk';
+
+process.env.NODE_ENV = 'production';
+
+/* eslint-disable no-console */
+
+console.log(chalk.green('Generating minified bundle for production. This will take a moment...'));
+
+webpack(webpackConfig).run((err, stats) => {
+	if(err){
+		//so a fatal error has occured. Stop here
+		console.log(chalk.red(err));
+		return 1;
+	}
+
+	const jsonStats = stats.toJson();
+
+	if(jsonStats.hasErrors){
+		return jsonStats.errors.map(error => console.log(chalk.red(error)));
+	}
+
+	if(jsonStats.hasWarnings){
+		console.log(chalk.yellow('Webpack generated the following warnings'));
+		jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)));
+	}
+
+	console.log(`Webpack stats: ${stats}`);
+
+	console.log(chalk.green('Your app has been built for production and written to /dist!'));
+
+	return 0;
+});
---
+++ buildScripts/distServer.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+//GZIP Compression
+app.use(compression());
+//Static files
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname,  '../dist/index.html'));
+});
+
+app.get('/users', (req, res) => {
+	res.json([
+		{ id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+		{ id: 2, firstName: "John", lastName: "Smith", email: "john@gmail.com" },
+		{ id: 3, firstName: "James", lastName: "Smith", email: "james@gmail.com" },
+	])
+});
+
+app.listen(port, (err) => {
+	if(err){
+		console.log(err);
+	}else{
+		open('http://localhost:' + port);
+	}
+})
