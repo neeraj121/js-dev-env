24bf8f4a4dea9bab718db841de1cc93ef42f7283
neeraj-grow <neeraj@grow.ae>
2020-04-21 21:09:10 +0400

    Dynamic HTML Generation

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 import path from 'path';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
 
 export default {
 	debug: true,
@@ -15,8 +16,28 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+		//Create HTML file that includes reference to bundled JS.
+		new HTMLWebpackPlugin({
+			template: 'src/index.html',
+			//minify html
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true
+			},
+			inject: true
+		}),
+
 		//Eliminate duplicate packages when generating bundle
 		new webpack.optimize.DedupePlugin(),
+
 		//Minify JS
 		new webpack.optimize.UglifyJsPlugin()
 	],
